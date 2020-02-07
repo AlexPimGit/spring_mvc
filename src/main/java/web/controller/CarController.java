4303805f464a7f3b83c64c8460d7d6ffa639588b
Alex Pim <shurik__@tut.by>
2020-02-07 18:30:17 +0300

    v1.3

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import web.model.Car;
+import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +15,11 @@ import java.util.List;
 @RequestMapping("/")
 public class CarController {
     @Autowired
-    private List<Car> carList;
+    private CarServiceImpl carService;
 
     @RequestMapping(value = "cars", method = RequestMethod.GET)//Value — это адрес в браузере; Method — метод запроса
         public String printCars(ModelMap model) {
-        model.addAttribute("carList", carList);
+        model.addAttribute("carList", carService.getCars());
         return "cars";//вернет строку-название jsp страницы
     }
 }
